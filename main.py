9c42b601e637ef889eea6dbf5277c21fa814b3c1
zik <isaacbull122@outlook.com>
2024-08-26 01:46:05 -0400

    added chat model to app and modified readme

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import os
 from dotenv import load_dotenv, dotenv_values
+import google.generativeai as genai
 
 
 load_dotenv()
 
-print(os.getenv("GEMINI_API_KEY"))
-print("hello world")
 
 
-def add(x, y):
-    return x + y
+def load_gemini_model():
+    genai.configure(api_key=os.environ['GEMINI_API_KEY'])
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    return model
+
+model = load_gemini_model()
+analyzePrompt = """You will be generating advises for whenever you are prompted like a chatbot
+who is a mental therapist, you will advise youths on how to navigate their adult life by asking 
+essential questions and giving quality advise"""
+
+response = model.generate_content(analyzePrompt)
